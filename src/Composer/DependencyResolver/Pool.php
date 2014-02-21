69e6e82887596aa4228eb97c77075677ffdd7049
Nils Adermann <naderman@naderman.de>
2014-02-21 14:59:41 +0100

    Use elseif instead of else { if {

--- src/Composer/DependencyResolver/Pool.php
+++ src/Composer/DependencyResolver/Pool.php
@@ -273,8 +273,7 @@ class Pool
             if (isset($this->packageByExactName[$name])) {
                 $candidates = array_merge($candidates, $this->packageByExactName[$name]);
             }
-        } else {
-            if (isset($this->packageByName[$name])) {
+        } elseif (isset($this->packageByName[$name])) {
                 $candidates = array_merge($candidates, $this->packageByName[$name]);
             }
         }
